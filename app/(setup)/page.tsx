649611fc36716b5ac35fe1f3931602879a221083
Yuta Kusuno <yutakusuno51@gmail.com>
2023-09-11 23:55:15 -0700

    feat: Add an initial modal to create a customize server

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
   const profile = await initialProfile();
@@ -18,7 +19,7 @@ const SetupPage = async () => {
   if (server) {
     return redirect(`/servers/${server.id}`);
   }
-  return <div>create a server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
