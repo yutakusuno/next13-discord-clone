d389f564a000d473365d8fb165a85869bd31d62f
Yuta Kusuno <yutakusuno51@gmail.com>
2023-09-12 17:22:04 -0700

    feat: Upload serverImage

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import axios from "axios";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
 
 import {
   Dialog,
@@ -19,10 +21,11 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "../ui/form";
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import FileUpload from "@/components/file-upload";
+import { useRouter } from "next/navigation";
 
 const fromSchema = z.object({
   name: z.string().min(1, {
@@ -35,6 +38,8 @@ const fromSchema = z.object({
 
 export const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false);
+
+  const router = useRouter();
 
   useEffect(() => {
     setIsMounted(true);
@@ -50,6 +55,13 @@ export const InitialModal = () => {
 
   const isLoading = form.formState.isSubmitting;
   const onSubmit = async (values: z.infer<typeof fromSchema>) => {
+    try {
+      await axios.post("/api/servers", values);
+      form.reset();
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
     console.log(values);
   };
 
@@ -71,9 +83,22 @@ export const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                Todo: image upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
-
               <FormField
                 control={form.control}
                 name="name"
